434a330c8bd60e2070fe90010755209107e022a3
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-09 13:49:23 -0500

    Creating User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,37 @@
 import { Schema,model } from 'mongoose'
 
 const userSchema = new Schema({
-    
+    displayName: String,
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    username: {
+        type: String,
+        minLength: 4,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        maxLength: 512
+    },
+    roles: [
+        {
+            ref: "Role",
+            type: Schema.Types.ObjectId
+        }
+    ],
+    status: {
+        type: Boolean,
+        default: true
+    }
 },{
     timestamps: true,
     versionKey: false
