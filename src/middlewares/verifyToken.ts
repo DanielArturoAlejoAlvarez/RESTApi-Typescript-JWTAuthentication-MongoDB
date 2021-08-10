2f6072d95c769d3f40b36f29bd30e160d1cca04d
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-10 13:41:23 -0500

    Creating IPayload interface

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -2,7 +2,11 @@ import config from "../config/config";
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-
+export interface IPayload {
+    id: string;
+    iat: number;
+    exp: number;
+}
 
 export const isAuth = (req: Request, res: Response, next: NextFunction) => {
   try {
