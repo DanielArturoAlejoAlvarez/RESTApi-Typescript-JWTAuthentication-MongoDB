03115152baa0dd13e9aad0fd86622198514ac177
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-10 15:26:37 -0500

    Apply check validations in signup router using our middlewares

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
-
+import {
+  checkRolesExists,
+  checkUsernameAndEmailExists,
+} from "../middlewares/checkSignUp";
 import { isAuth } from "../middlewares/verifyToken";
 import { profile, signIn, signUp } from "../controllers/auth/auth.controller";
 
 const router: Router = Router();
 
 router.post("/signin", signIn);
-router.post("/signup", signUp);
+router.post("/signup", [checkRolesExists, checkUsernameAndEmailExists], signUp);
 router.get("/profile", isAuth, profile);
 
 export default router;
