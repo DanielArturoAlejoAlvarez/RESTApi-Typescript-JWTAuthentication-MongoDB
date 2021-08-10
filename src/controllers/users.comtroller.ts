1a885cbf4807b60acc157f072074cf489f08ab2b
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-10 15:29:22 -0500

    Creating getUsers method in users controller

--- src/controllers/users.comtroller.ts
+++ src/controllers/users.comtroller.ts
@@ -1,11 +1,9 @@
-import { Request,Response } from 'express'
-import User from '../models/User'
+import { Request, Response } from "express";
+import User from "../models/User";
 
-export const getUsers = async (req: Request,res: Response)=>{
+export const getUsers = async (req: Request, res: Response) => {
+  const users = await User.find().populate("roles");
+  return res.json(users);
+};
 
-
-}
-
-export const getUser = async (req: Request,res: Response)=>{
-    
-}
+export const getUser = async (req: Request, res: Response) => {};
