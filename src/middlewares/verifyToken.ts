d995396a0ceb5f52dfdc69e41a2872f477706ff4
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-10 13:44:03 -0500

    Creating isAuth middleware using jsonwebtoken verify

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -10,8 +10,23 @@ export interface IPayload {
 
 export const isAuth = (req: Request, res: Response, next: NextFunction) => {
   try {
-    
+    const token = req.header("auth-token");
+
+    if (!token)
+      return res.status(401).json({
+        msg: "Access denied!",
+      });
+
+    const payload = jwt.verify(token, config.secret_key) as IPayload;
+
+    req.userId = payload.id
+
+    console.log(payload);
+    console.log(req.userId)
+
+    next();
   } catch (err) {
-    
+    console.log(err);
+    return res.status(500).json(err);
   }
 };
