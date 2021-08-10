eb0537eb2c0f8ec9dbdbb3352656d8201dc3cac5
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-09 23:57:00 -0500

    Creating signIn method in User controller

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,58 +1,78 @@
-import config from '../../config/config'
+import config from "../../config/config";
 
-import User,{IUser} from '../../models/User'
-import { Request,Response } from 'express'
-import jwt from 'jsonwebtoken'
-import Role from 'models/Role'
+import User, { IUser } from "../../models/User";
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import Role from "models/Role";
 
-export const signUp = async (req: Request,res: Response)=>{
+export const signUp = async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    const { displayName, email, username, password, avatar, roles, status } =
+      req.body;
+    const newUser: IUser = new User({
+      displayName,
+      email,
+      username,
+      password,
+      avatar,
+      status,
+    });
 
-    try {
-        console.log(req.body)
-        const {displayName,email,username,password,avatar,roles,status} = req.body
-        const newUser: IUser = new User({
-            displayName,
-            email,
-            username,
-            password,
-            avatar,
-            status
-        })
+    newUser.encryptPassword(password);
 
-        newUser.encryptPassword(password)
-
-        //roles
-        if (req.body.roles) {
-            const arrayRoles = await Role.find({name: {$in: roles}})
-            newUser.roles = arrayRoles.map((role: any)=>role._id)
-        }else {
-            const role = Role.findOne({name: "USER"})
-            newUser.roles = [role._id]
-        }
-
-        const user = await newUser.save()
-        console.log(user)
-
-        const token = jwt.sign({id: user._id}, config.secret_key,{
-            expiresIn: 60*60
-        })
-
-        return res.status(200).json({
-            msg: 'User registered successfully!',
-            token,
-            user
-        })
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json(err)
+    //roles
+    if (req.body.roles) {
+      const arrayRoles = await Role.find({ name: { $in: roles } });
+      newUser.roles = arrayRoles.map((role: any) => role._id);
+    } else {
+      const role = Role.findOne({ name: "USER" });
+      newUser.roles = [role._id];
     }
 
-}
+    const user = await newUser.save();
+    console.log(user);
 
-export const signIn = async (req: Request,res: Response)=>{
-    
-}
+    const token = jwt.sign({ id: user._id }, config.secret_key, {
+      expiresIn: 60 * 60,
+    });
 
-export const profile = async (req: Request,res: Response)=>{
-    
-}
+    return res.status(200).json({
+      msg: "User registered successfully!",
+      token,
+      user,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+};
+
+export const signIn = async (req: Request, res: Response) => {
+    const user = await User.findOne({email: req.body.email})
+    if (!user) {
+        return res.status(400).json({
+            msg: 'User not found!'
+        })
+    }
+    const matchPassword = await user.validatePassword(req.body.password, user.password)
+
+    if (!matchPassword) {
+        return res.status(401).json({
+            token: null,
+            msg: 'Password is invalid!'
+        })
+    }
+
+    const token = jwt.sign({id: user._id}, config.secret_key, {
+        expiresIn: 60*60
+    })
+
+    return res.status(200).json({
+        msg: 'User is loggedin!',
+        token
+    })
+
+};
+
+export const profile = async (req: Request, res: Response) => {};
