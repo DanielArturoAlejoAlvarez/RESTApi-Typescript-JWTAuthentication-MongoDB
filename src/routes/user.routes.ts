45876ddc584883657ee919ff73fbbdc3f779cfda
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-11 11:13:23 -0500

    Adding middlewares to save users in routing

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { getUser, getUsers } from "../controllers/users.controller";
+import { isAuth } from "../middlewares/verifyToken";
+import { getUser, getUsers, saveUser } from "../controllers/users.controller";
+import { checkRolesExists } from "../middlewares/checkSignUp";
+import { isAdmin, isSuperAdmin } from "../middlewares/permissionsLevel";
 
 const router: Router = Router();
 
 router.get("/", getUsers);
 router.get("/:idUser", getUser);
-
+router.post("/", [isAuth, checkRolesExists, isSuperAdmin, isAdmin], saveUser);
 
 export default router;
