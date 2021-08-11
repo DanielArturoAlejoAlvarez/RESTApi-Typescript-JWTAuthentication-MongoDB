4ed5ae296c0c1da7cf5e88bcd7dfbb208b52dc6d
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-11 14:53:18 -0500

    Update .env

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,8 @@
-import config from "./config";
+//import config from "./config";
 import mongoose from "mongoose";
 
 mongoose
-  .connect(config.mongodb_uri, {
+  .connect(process.env.MONGO_DB || 'mongodb://127.0.0.1:27017/example', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import app from "./app";
 import "./config/database";
 
