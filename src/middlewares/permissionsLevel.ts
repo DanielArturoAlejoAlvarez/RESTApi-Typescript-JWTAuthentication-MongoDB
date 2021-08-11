70c372611d2416edd199e2671530d9759597cdb9
Daniel Arturo Alejo Alvarez <yourmediasoft@gmail.com>
2021-08-11 10:54:31 -0500

    Creating isAdmin middleware in permissions level

--- src/middlewares/permissionsLevel.ts
+++ src/middlewares/permissionsLevel.ts
@@ -23,3 +23,24 @@ export const isSuperAdmin = async (req: Request,res: Response,next: NextFunction
         return res.status(500).json(err)
     }
 }
+
+export const isAdmin = async (req: Request,res: Response,next: NextFunction)=>{
+    try {
+        const user = await User.findById(req.userId)
+
+        const roles = await Role.findOne({_id: {$in: user?.roles}})
+
+        for (let i = 0; i < roles.length; i++) {
+            if (roles[i].name==='ADMIN') {
+                next()
+                return
+            }      
+        }
+        return res.status(403).json({
+            msg: 'Require ADMIN Role!'
+        })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+}
